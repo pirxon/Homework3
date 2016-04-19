181c8ba947b60af7ceac79dfed674526103202d2
pirxon <pirx@ukr.net>
2016-04-19 12:19:52 +0300

    Added files via upload

---
+++ Zadanie1.java
@@ -0,0 +1,24 @@
+package homework3;
+
+public class Zadanie1 {
+	protected double n;
+
+	public double getN() {
+		return n;
+	}
+
+	public void setN(double n) {
+		this.n = n;
+	}
+	
+	public void summa() {
+		int temp = (int)(n * 100);
+		int sum = 0; 
+		while (temp > 0) {
+			sum = sum + temp % 10;
+			temp = temp / 10;
+		}
+		System.out.println("Сумма чисел - "+sum);
+		
+	}
+}
