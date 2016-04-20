da204203ec0f3cc015118c5f87fc462a0a00b31f
Vadim <pirx@ukr.net>
2016-04-20 15:24:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,15 +3,16 @@ package homework3;
 public class Main {
 
 	public static void main(String[] args) {
-
-		Zadanie1 chislo = new Zadanie1();
-		chislo.setN(321.65);
-		chislo.summa();
 		
+		Zadanie1 chislo = new Zadanie1();
+		chislo.setN(321.28);
+		chislo.summa();
+
 		Zadanie2 chislo_max_min = new Zadanie2();
 		chislo_max_min.setN(234.56);
 		chislo_max_min.max();
 		chislo_max_min.min();
 		
+	 	
 	}
 }
