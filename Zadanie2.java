15e577379b6b9dab7f5f1c671875ef89baad4e6e
Vadim <pirx@ukr.net>
2016-04-22 17:44:23 +0300

    Update Zadanie2.java

--- Zadanie2.java
+++ Zadanie2.java
@@ -3,55 +3,61 @@ package homework3;
 public class Zadanie2 {
 	private double n;
 
-	public double getN() {
-		return n;
-	}
-
 	public void setN(double n) {
 		this.n = n;
 	}
+
 	public void max() {
-		//int temp = (int)(n * 100);
-		int temp =(int)(Math.round(n * 100));
-	 	
-	    int a = (temp % 100000 / 10000);
-	 	int b = (temp % 10000 / 1000);
-	    int c = (temp % 1000 / 100);
-	    int d = (temp % 100 / 10);
-	    int e = (temp % 10);
+		int temp = (int) (Math.round(n * 100));
 
-		if(a>b & a>c & a>d & a>e) {
-			System.out.println("Максимальное число - "+a);
-		}else if(b>c & b>d & b>e ) {
-			System.out.println("Максимальное число - "+b);
-		}else if(c>d & c>e) {
-			System.out.println("Максимальное число - "+c);
-		}else if (d>e){
-			System.out.println("Максимальное число - "+d);
-		}else
-			System.out.println("Максимальное число - "+e);
+		int a = (temp % 100000 / 10000);
+		int b = (temp % 10000 / 1000);
+		int c = (temp % 1000 / 100);
+		int d = (temp % 100 / 10);
+		int e = (temp % 10);
+
+		int max = Math.max(a, Math.max(b, Math.max(c, Math.max(d, e))));
+		System.out.println("Максимальное число - " + max);
+
+		/* второй вариант
+		 * if(a>b & a>c & a>d & a>e) { 
+		 * System.out.println("Максимальное число - "+a); 
+		 * }else if(b>c & b>d & b>e ) {
+		 * System.out.println("Максимальное число - "+b); 
+		 * }else if(c>d & c>e) {
+		 * System.out.println("Максимальное число - "+c); 
+		 * }else if (d>e){
+		 * System.out.println("Максимальное число - "+d); 
+		 * }else
+		 * System.out.println("Максимальное число - "+e);
+		 */
 	}
-	
+
 	public void min() {
-		//int temp = (int)(n * 100);
-		int temp =(int)(Math.round(n * 100));
-	 	
-	    int a = (temp % 100000 / 10000);
-	 	int b = (temp % 10000 / 1000);
-	    int c = (temp % 1000 / 100);
-	    int d = (temp % 100 / 10);
-	    int e = (temp % 10);
+		// int temp = (int)(n * 100);
+		int temp = (int) (Math.round(n * 100));
 
-		if(a<b & a<c & a<d & a<e) {
-			System.out.println("Минимальное число - "+a);
-		}else if(b<c & b<d & b<e ) {
-			System.out.println("Минимальное число - "+b);
-		}else if(c<d & c<e) {
-			System.out.println("Минимальное число - "+c);
-		}else if (d<e){
-			System.out.println("Минимальное число - "+d);
-		}else
-			System.out.println("Минимальное число - "+e);
-		
+		int a = (temp % 100000 / 10000);
+		int b = (temp % 10000 / 1000);
+		int c = (temp % 1000 / 100);
+		int d = (temp % 100 / 10);
+		int e = (temp % 10);
+
+		int min = Math.min(a, Math.min(b, Math.min(c, Math.min(d, e))));
+		System.out.println("Максимальное число - " + min);
+
+		/* второй вариант
+		 * if(a<b & a<c & a<d & a<e) { 
+		 * System.out.println("Минимальное число - " + a); 
+		 * }else if(b<c & b<d & b<e ) { 
+		 * System.out.println("Минимальное число - "+ b);
+		 *  }else if(c<d & c<e) { 
+		 *  System.out.println("Минимальное число - "+ c); 
+		 *  }else if (d<e){ 
+		 *  System.out.println("Минимальное число - "+ d); 
+		 *  }else 
+		 *  System.out.println("Минимальное число - "+ e);
+		 */
 	}
+
 }
